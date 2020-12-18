30c809a76d9c1aea3dc763e0c6f048c71e0dde09
cristian929 <cristianlucas180@gmail.com>
2020-12-18 16:40:26 -0300

    prova

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <p>S
+        CRISTIAN LUCAS
+    </p>
+    <p>
+        <a href="https://github.com/cristian929"> GIT HUB </a>
+    </p>
+    <p>
+        <a href="https://www.linkedin.com/in/cristian-lucas-8650ba1a3/"> LINKEDIN </a>
+    </p>
+    <p>
+    Prova da ummense - MYSQL,PHP,LARAVEL,VUE.JS &copy; 2020
+    </p>
+</footer>
